3c6adbe578bbd7c3a66d49fc93129e3aeb377922
Ripples <37956697+ripplesaround@users.noreply.github.com>
2021-11-16 19:30:16 +0800

    Add files via upload
    init

---
+++ co-teaching-labelacc/training.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES='2' python3 main.py \
+        --dataset cifar10 \
+        --noise_type symmetric  \
+        --noise_rate 0.5    \
+        --forget_rate 0.5       \
+        --n_epoch 40
